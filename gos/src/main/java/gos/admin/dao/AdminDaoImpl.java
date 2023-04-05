da4904cb3b7a7f61d9f8aeffafc0482dcad61add
Seung Woo Park <ndm05006@gmail.com>
2023-04-05 09:57:39 +0900

    mapper, dao 블랙리스트, 신고제외 커밋

--- gos/src/main/java/gos/admin/dao/AdminDaoImpl.java
+++ gos/src/main/java/gos/admin/dao/AdminDaoImpl.java
@@ -1,44 +1,177 @@
 package gos.admin.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import gos.util.MybatisSqlSessionFactory;
 import gos.vo.MemberVO;
 import gos.vo.NoticeBoardVO;
 import gos.vo.RestautantVO;
 
-public interface AdminDaoImpl {
+public class AdminDaoImpl implements IAdminDao{
+
 	
-	//-------사업자 승인 페이지 ------------------
-	public RestautantVO waitApproval();
+	//-------사업자 승인 페이지 ------------------ 
 	
-	public int signupApproval(String res_id);
-	
-	public int signupDelete(String res_id);
+	// 사업자 정보조회(승인대기)
+	@Override
+	public RestautantVO waitApproval() {
 		
+		RestautantVO vo = null;
+		
+		SqlSession session = null;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			
+			
+		} finally {
+			// TODO: handle finally clause
+		}
+		
+		return vo;
+	}
+	
+	// 결재관리버튼(승인)
+	@Override
+	public int signupApproval(String res_id) {
+		
+		SqlSession session = null;
+		int cnt =0;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			
+			
+		} finally {
+			// TODO: handle finally clause
+		}		
+		
+		return 0;
+	}
+
+	// 결재관리버튼(삭제)
+	@Override
+	public int signupDelete(String res_id) {
+		SqlSession session = null;
+		int cnt =0;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			
+			
+		} finally {
+			// TODO: handle finally clause
+		}		
+		
+		return 0;
+	}
+	
 	
 	
 	//--------- 회원관리----------------------
-	
-	public RestautantVO restaurantInfo();
+	// 사업자 정보 조회 (관리자)
+	@Override
+	public RestautantVO restaurantInfo() {
 
-	public RestautantVO restaurantDetail(String rst_id);
+		RestautantVO vo = null;
+		
+		SqlSession session = null;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			
+			
+		} finally {
+			// TODO: handle finally clause
+		}
+		
+		return vo;
+	}
+
+	// 사업자 상세 조회
+	@Override
+	public RestautantVO restaurantDetail(String rst_id) {
+		
+		RestautantVO vo = null;
+		
+		SqlSession session = null;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			
+			
+		} finally {
+			// TODO: handle finally clause
+		}
+		
+		return vo;
+	}
+
+	// 멤버 조회
+	@Override
+	public MemberVO memberInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	// 멤버 상세조회
+	@Override
+	public MemberVO memberDetail(String mem_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	
 	
-	public MemberVO memberInfo();
 	
-	public MemberVO memberDetail(String mem_id);
+	
+	
+	// 멤버 블랙리스트 관리부분
+	
 	
 	
 	
 	//---------- 공지사항 게시판
-	public NoticeBoardVO noticeBoardInfo();
 	
+	// 공지사항 전체 조회
+	@Override
+	public NoticeBoardVO noticeBoardInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//선택한 공지사항 조회
+	@Override
+	public NoticeBoardVO noticeBoardDetail(int nb_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-	public NoticeBoardVO noticeBoardDetail(int nb_id);
+	//공지사항 입력
+	@Override
+	public int noticeBoardWrite(NoticeBoardVO vo) {
+		SqlSession session = null;
+		int cnt =0;
+		
+		try {
+			session = MybatisSqlSessionFactory.getSqlSession();
+			
+			
+		} finally {
+			// TODO: handle finally clause
+		}		
+		
+		return 0;
+	}
 	
+	// 공지사항 삭제
+	@Override
+	public NoticeBoardVO signupDelete(int nb_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
-	public int noticeBoardWrite(NoticeBoardVO vo);
-	
-	
-	public
-	
+	// 공지사항 수정자리
 	
 }
--- gos/src/main/java/gos/admin/dao/IAdminDao.java
+++ gos/src/main/java/gos/admin/dao/IAdminDao.java
@@ -1,5 +1,106 @@
 package gos.admin.dao;
 
-public class IAdminDao {
+import gos.vo.MemberVO;
+import gos.vo.NoticeBoardVO;
+import gos.vo.RestautantVO;
 
+public interface IAdminDao {
+	//-------사업자 승인 페이지 ------------------
+	
+	/**
+	 * 사업자 정보조회(승인대기)
+	 * @return RestautantVO
+	 */
+	public RestautantVO waitApproval();
+	
+	/**
+	 * 결재관리버튼(승인)
+	 * @param res_id 레스토랑 아이디
+	 * @return 성공1 실패0
+	 */
+	public int signupApproval(String res_id);
+	
+	
+	
+	/**
+	 * 결재관리버튼(삭제)
+	 * @param res_id 레스토랑 아이디
+	 * @return 성공1 실패1
+	 */
+	public int signupDelete(String res_id);
+		
+	
+	
+	//--------- 회원관리----------------------
+	
+	/**
+	 * 회원관리 페이지
+	 * 사업자 정보 조회 전체
+	 * @return RestautantVO
+	 */
+	public RestautantVO restaurantInfo();
+
+	/**
+	 * 회원관리 페이지
+	 * 클릭한 사업자 자세하게 조회 
+	 * @param rst_id
+	 * @return
+	 */
+	public RestautantVO restaurantDetail(String rst_id);
+	
+	/**
+	 * 회원관리 페이지
+	 * 회원 전체 정보 조회
+	 * @return RestautantVO
+	 */
+	public MemberVO memberInfo();
+	
+	
+	/**
+	 * 회원관리 페이지
+	 * 클릭한 회원 자세하게 조회 
+	 * @param rst_id
+	 * @return
+	 */
+	public MemberVO memberDetail(String mem_id);
+	
+	
+	// 멤버 블랙리스트 관리부분
+	
+	
+	
+	
+	//---------- 공지사항 게시판
+	
+	/**
+	 * 공지사항 게시판
+	 * 게시판 전체 조회
+	 * @return RestautantVO
+	 */
+	public NoticeBoardVO noticeBoardInfo();
+	
+	
+	/**
+	 * 공지사항 게시판
+	 * 클릭한 게시판 조회
+	 * @return RestautantVO
+	 */
+	public NoticeBoardVO noticeBoardDetail(int nb_id);
+	
+	/**
+	 * 공지사항 게시판
+	 * 공지사항 입력
+	 * @return 성공1 실패0
+	 */
+	public int noticeBoardWrite(NoticeBoardVO vo);
+	
+	/**
+	 * 공지사항 게시판
+	 * 공지사항 삭제
+	 * @return 성공1 실패0
+	 */
+	public NoticeBoardVO signupDelete(int nb_id);
+	
+	
+	// 공지사항 수정자리
 }
